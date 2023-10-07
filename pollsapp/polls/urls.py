66cbdb5728ab6d809ac6155d0859e0d8525e289e
xenonzenon <jayson_abad@outlook.com>
2023-10-08 05:35:02 +0800

    improved views

--- pollsapp/polls/urls.py
+++ pollsapp/polls/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name="polls-index"),
-	path('<int:question_id>/', views.detail, name='detail'),
-	path('<int:question_id>/results/', views.result, name='results'),
-	path('<int:question_id>/vote/', views.vote, name='vote'),
+	path('polls/', views.index, name="polls-index"),
+	path('polls/<int:question_id>/', views.detail, name='detail'),
+	path('polls/<int:question_id>/results/', views.result, name='results'),
+	path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+from . models import Question
 
 def index(request):
-	return HttpResponse("<h1>Hello World!</h1>")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = { 'latest_question_list':latest_question_list }
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("<p>You are looking at question %s.</p>" % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist.")
+	return render(request, 'polls/detail.html', {'question':question})
 
 def result(request, question_id):
 	response = "<p>You're looking at the results of question %s.</p>"
