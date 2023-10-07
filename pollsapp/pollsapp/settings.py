1fa3852689c8b215f62e5c4cb394f37aa91e553b
xenonzenon <jayson_abad@outlook.com>
2023-10-08 00:46:50 +0800

    added database

--- pollsapp/pollsapp/settings.py
+++ pollsapp/pollsapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pollsapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pollsappdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 8000
     }
 }
 
