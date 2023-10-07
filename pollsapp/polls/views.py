c63300e94c5d425b3517e5cb271efd150b4c4190
xenonzenon <jayson_abad@outlook.com>
2023-10-08 02:21:47 +0800

    added some for polls views

--- pollsapp/polls/views.py
+++ pollsapp/polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("<h1>Hello World!</h1>")
+
+def detail(request, question_id):
+	return HttpResponse("<p>You are looking at question %s.</p>" % question_id)
+
+def result(request, question_id):
+	response = "<p>You're looking at the results of question %s.</p>"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("<p>You are voting on question %s.</p>" % question_id)
