1b5a75c9a6badb14ef065517e94836d48ade3df4
xenonzenon <jayson_abad@outlook.com>
2023-10-11 17:52:38 +0800

    added shelf app

---
+++ shelf/books/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Author, Books, Publisher, User
+
+admin.site.register(Author)
+admin.site.register(Books)
+admin.site.register(Publisher)
+admin.site.register(User)
